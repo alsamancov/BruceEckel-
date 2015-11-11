c15400d99d83141bfd8ff85a9b57ae573662a6c0
alsamancov <alsamancov@gmail.com>
2015-11-11 15:17:24 +0200

    second commit

---
+++ src/main/java/Chapter06/lesson01/SingleImport01.java
@@ -0,0 +1,10 @@
+package Chapter06.lesson01;
+
+
+import java.util.ArrayList;
+
+public class SingleImport01 {
+    public static void main(String[] args){
+        ArrayList list = new java.util.ArrayList();
+    }
+}
---
+++ src/main/java/Chapter06/lesson01/SingleImport02.java
@@ -0,0 +1,12 @@
+package Chapter06.lesson01;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Алексей on 11/11/2015.
+ */
+public class SingleImport02 {
+    public static void main(String[] args){
+        ArrayList list = new java.util.ArrayList();
+    }
+}
