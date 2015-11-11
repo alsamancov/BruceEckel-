99e5631c820141c068691bc33e30a62f99248a2c
alsamancov <alsamancov@gmail.com>
2015-11-11 15:28:39 +0200

    second commit

---
+++ src/main/java/Chapter06/lesson02/QualifiedMyClass02.java
@@ -0,0 +1,10 @@
+package Chapter06.lesson02;
+
+/**
+ * Created by Алексей on 11/11/2015.
+ */
+public class QualifiedMyClass02 {
+    public static void main(String[] args){
+        Chapter06.lesson02.mypackage.MyClass02 m = new Chapter06.lesson02.mypackage.MyClass02();
+    }
+}
