f9378403bdd252c705d79b6360dbc51229f95300
alsamancov <alsamancov@gmail.com>
2015-11-13 17:27:05 +0200

    add classes for lesson03

---
+++ src/main/java/Chapter06/lesson03/net/mindview/simple/List04.java
@@ -0,0 +1,10 @@
+package Chapter06.lesson03.net.mindview.simple;
+
+/**
+ * Created by Алексей on 11/13/2015.
+ */
+public class List04 {
+    public List04(){
+        System.out.println("Chapter06.lesson03.net.mind.view.List04");
+    }
+}
--- src/main/java/Chapter06/lesson03/net/mindview/simple/Vector07.java
+++ src/main/java/Chapter06/lesson03/net/mindview/simple/Vector07.java
@@ -1,4 +1,4 @@
-package Chapter06.lesson03;
+package Chapter06.lesson03.net.mindview.simple;
 
 /**
  * Created by Алексей on 11/13/2015.
