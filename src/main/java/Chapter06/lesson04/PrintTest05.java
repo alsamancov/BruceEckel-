11d931851b32e1e43ee3a7a912da682622d3cb27
alsamancov <alsamancov@gmail.com>
2015-11-14 14:49:13 +0200

    add classes for lesson05

---
+++ src/main/java/Chapter06/lesson04/PrintTest05.java
@@ -0,0 +1,15 @@
+package Chapter06.lesson04;
+
+import static Chapter06.lesson04.Print04.*;
+
+/**
+ * Created by Alexey on 11/14/2015.
+ */
+public class PrintTest05 {
+    public static void main(String[] args){
+        print("Available from now on!");
+        print(100);
+        print(100L);
+        print(3.14159);
+    }
+}
---
+++ src/main/java/Chapter06/lesson05/Dinner01.java
@@ -0,0 +1,12 @@
+package Chapter06.lesson05;
+
+import Chapter06.lesson05.dessert.Cookie01;
+
+/**
+ * Created by Alexey on 11/14/2015.
+ */
+public class Dinner01 {
+    public static void main(String[] args){
+        Cookie01 x = new Cookie01();
+    }
+}
