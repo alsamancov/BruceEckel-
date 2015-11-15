f1b0343b435ac55b045fa8fcf3b01a42feab1abf
Alex <alsamancov@gmail.com>
2015-11-15 14:58:21 +0200

    add classes for lesson08

---
+++ src/main/java/Chapter06/lesson08/task02/Lunch02.java
@@ -0,0 +1,33 @@
+package Chapter06.lesson08.task02;
+
+/**
+ * Created by Alex on 15.11.15.
+ */
+class Soup1_02{
+    private Soup1_02(){}
+    public static Soup1_02 makeSoup(){
+        return new Soup1_02();
+    }
+}
+
+class Soup2_02{
+    private Soup2_02(){}
+
+    private static Soup2_02 ps1 = new Soup2_02();
+    public static Soup2_02 access(){
+        return ps1;
+    }
+    public void f(){}
+}
+public class Lunch02 {
+    void testPrivate(){
+        //Soup1_02 soup = new Soup1_02();
+    }
+
+    void testStatic(){
+        Soup1_02 soup1_02 = Soup1_02.makeSoup();
+    }
+    void testSingleton(){
+        Soup2_02.access().f();
+    }
+}
