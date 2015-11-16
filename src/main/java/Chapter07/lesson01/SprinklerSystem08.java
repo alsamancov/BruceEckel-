8235f59770d80b9aa9b6017f5061e8be2fde06a5
Alex <alsamancov@gmail.com>
2015-11-16 21:22:08 +0200

    added classes in Ch07 les01

---
+++ src/main/java/Chapter07/lesson01/SprinklerSystem08.java
@@ -0,0 +1,33 @@
+package Chapter07.lesson01;
+
+/**
+ * Created by Alex on 16.11.15.
+ */
+class WaterSource08{
+    private String s;
+    WaterSource08(){
+        System.out.println("WaterSource08()");
+        s = "Constructed";
+    }
+    public String toString(){
+        return s;
+    }
+}
+public class SprinklerSystem08 {
+    private String valve1, valve2, valve3, valve4;
+    private WaterSource08 source08 = new WaterSource08();
+    private int i;
+    private float f;
+    public String toString(){
+        return "valve1 = " + valve1 + " " +
+                "valve2 = " + valve2 + " " +
+                "valve3 = " + valve3 + " " +
+                "valve4 = " + valve4 + "\n" +
+                "i = " + i + " " + "f = " + f + " " +
+                "source = " + source08;
+    }
+    public static void main(String[] args){
+        SprinklerSystem08 sprinklerSystem08 = new SprinklerSystem08();
+        System.out.println(sprinklerSystem08);
+    }
+}
---
+++ src/main/java/Chapter07/lesson01/SprinklerSystem10.java
@@ -0,0 +1,35 @@
+package Chapter07.lesson01;
+
+/**
+ * Created by Alex on 16.11.15.
+ */
+
+class WaterSource10{
+    private String s;
+    WaterSource10(){
+        System.out.println("WaterSource10()");
+        s = "Constructed";
+    }
+    public String toString(){
+        return s;
+    }
+}
+
+public class SprinklerSystem10 {
+    private String valve1, valve2, valve3, valve4;
+    private WaterSource10 source10 = new WaterSource10();
+    private int i;
+    private float f;
+    public String toString(){
+        return "valve1 = " + valve1 + " " +
+                "valve2 = " + valve2 + " " +
+                "valve3 = " + valve3 + " " +
+                "valve4 = " + valve4 + "\n" +
+                "i = " + i + " " + "f = " + f + " " +
+                "source = " + source10;
+    }
+    public static void main(String[] args){
+        SprinklerSystem10 sprinklerSystem10 = new SprinklerSystem10();
+        System.out.println(sprinklerSystem10);
+    }
+}
