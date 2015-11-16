de417010ff0c0dc6f7fa68a3799ce1e38505cc7c
Alex <alsamancov@gmail.com>
2015-11-16 22:03:31 +0200

    added classes in Ch07 les02

---
+++ src/main/java/Chapter07/lesson02/Bath02.java
@@ -0,0 +1,46 @@
+package Chapter07.lesson02;
+
+/**
+ * Created by Alex on 16.11.15.
+ */
+class Soap02{
+    private String s;
+    Soap02(){
+        System.out.println("Soap02()");
+        s = "Constructed";
+    }
+    public String toString(){
+        return s;
+    }
+}
+public class Bath02 {
+    private String
+    s1 = "Happy",
+    s2 = "Happy",
+    s3, s4;
+    private Soap02 castille;
+    private int i;
+    private float toy;
+    public Bath02(){
+        System.out.println("Inside Bath02()");
+        s3 = "Joy";
+        toy = 3.14f;
+        castille = new Soap02();
+    }
+    { i = 47;}
+    public String toString(){
+        if(s4 == null)
+            s4 = "Joy";
+        return "s1 = " + s1 + "\n" +
+                "s2 = " + s2 + "\n" +
+                "s3 = " + s3 + "\n" +
+                "s4 = " + s4 + "\n" +
+                "i = " + i + "\n" +
+                "toy = " + toy + "\n" +
+                "castille = " + castille;
+    }
+    public static void main(String[] args){
+        Bath02 b = new Bath02();
+        System.out.println(b);
+    }
+}
