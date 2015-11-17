4b9bf03b1d25bd2292d2e8f1e1225e8b27360058
Alex <alsamancov@gmail.com>
2015-11-17 11:06:42 +0200

    added classes in Ch07 les02

---
+++ src/main/java/Chapter07/lesson02/Bath03.java
@@ -0,0 +1,21 @@
+package Chapter07.lesson02;
+
+/**
+ * Created by Alex on 17.11.15.
+ */
+
+class Soap03{
+    private String s;
+    Soap03(){
+        System.out.println("Soap03()");
+        s = "Constructed";
+    }
+    public String toString(){
+        return s;
+    }
+}
+
+public class Bath03 {
+
+
+}
