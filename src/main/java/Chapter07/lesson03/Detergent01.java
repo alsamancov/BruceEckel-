9c04df3590046232e670091f28a319adecd92ab4
alsamancov <alsamancov@gmail.com>
2015-11-17 17:28:20 +0200

    add classes for ch07 les03

---
+++ src/main/java/Chapter07/lesson03/Detergent01.java
@@ -0,0 +1,47 @@
+package Chapter07.lesson03;
+
+/**
+ * Created by Alexey on 11/17/2015.
+ */
+class Cleanser01{
+    private String s = "Cleanser";
+    public void append(String a){
+        s += a;
+    }
+    public void dilute(){
+        append(" dilute()");
+    }
+    public void apply(){
+        append(" apply()");
+    }
+    public void scrub(){
+        append(" scrub()");
+    }
+    public String toString(){
+        return s;
+    }
+    public static void main(String[] args){
+        Cleanser01 x = new Cleanser01();
+        x.dilute(); x.apply(); x.scrub();
+        System.out.println(x);
+    }
+}
+public class Detergent01 extends Cleanser01 {
+    public void scrub(){
+        append(" Detergent.scrub()");
+        super.scrub();
+    }
+    public void foam(){
+        append(" foam()");
+    }
+    public static void main(String[] args){
+        Detergent01 x = new Detergent01();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        System.out.println(x);
+        System.out.println("Testing base class:");
+        Cleanser01.main(args);
+    }
+}
---
+++ src/main/java/Chapter07/lesson03/Detergent02.java
@@ -0,0 +1,47 @@
+package Chapter07.lesson03;
+
+/**
+ * Created by Alexey on 11/17/2015.
+ */
+class Cleanser02{
+    private String s = "Cleanser02";
+    public void append(String a){
+        s += a;
+    }
+    public void dilute(){
+        append(" dilute()");
+    }
+    public void apply(){
+        append(" apply()");
+    }
+    public void scrub(){
+        append(" scrub()");
+    }
+    public String toString(){
+        return s;
+    }
+    public static void main(String[] args){
+        Cleanser02 x = new Cleanser02();
+        x.dilute(); x.apply(); x.scrub();
+        System.out.println(x);
+    }
+}
+public class Detergent02 extends Cleanser02{
+    public void scrub(){
+        append(" Detergent02.scrub()");
+        super.scrub();
+    }
+    public void foam(){
+        append(" foam()");
+    }
+    public static void main(String[] args){
+        Detergent02 x = new Detergent02();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        System.out.println(x);
+        System.out.println("Testing base class:");
+        Cleanser02.main(args);
+    }
+}
