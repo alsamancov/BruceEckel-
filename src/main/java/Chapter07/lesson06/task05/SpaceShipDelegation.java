5a14c00a1c6d7be661fdd03518084ad0c9b60536
alsamancov <alsamancov@gmail.com>
2015-11-18 15:51:28 +0200

    add classes for ch07 les06

---
+++ src/main/java/Chapter07/lesson06/task05/SpaceShipDelegation.java
@@ -0,0 +1,36 @@
+package Chapter07.lesson06.task05;
+
+/**
+ * Created by Alexey on 11/18/2015.
+ */
+public class SpaceShipDelegation {
+    private String name;
+    private SpaceShipControls controls =
+            new SpaceShipControls();
+    public SpaceShipDelegation(String name){
+        this.name = name;
+    }
+    public void back(int velocity){
+        controls.back(velocity);
+    }
+    public void forward(int velocity){
+        controls.forward(velocity);
+    }
+    public void left(int velocity){
+        controls.left(velocity);
+    }
+    public void right(int velocity){
+        controls.right(velocity);
+    }
+    public void turboBoost(){
+        controls.turboBoost();
+    }
+    public void up(int velocity){
+        controls.up(velocity);
+    }
+    public static void main(String[] args){
+        SpaceShipDelegation protector =
+                new SpaceShipDelegation("NSEA Protector");
+        protector.forward(100);
+    }
+}
