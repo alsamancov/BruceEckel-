758b3a7f54d56478677ab72f546fae57e9043f14
alsamancov <alsamancov@gmail.com>
2015-11-18 16:23:30 +0200

    add classes for ch07 les06

---
+++ src/main/java/Chapter07/lesson06/task07/SpaceShipControls.java
@@ -0,0 +1,14 @@
+package Chapter07.lesson06.task07;
+
+/**
+ * Created by Alexey on 11/18/2015.
+ */
+public class SpaceShipControls {
+    void up(int velocity){}
+    void down(int velocity){}
+    void left(int velocity){}
+    void right(int velocity){}
+    void forward(int velocity){}
+    void back(int velocity){}
+    void turboBoost(){}
+}
