36b317118694fa512c8523b49fcc070dce18918b
alsamancov <alsamancov@gmail.com>
2015-11-19 16:17:35 +0200

    add classes for ch06 les03

---
+++ src/main/java/Chapter06/lesson03/net/mindview/simple02/LibTest10.java
@@ -0,0 +1,11 @@
+package Chapter06.lesson03.net.mindview.simple02;
+
+/**
+ * Created by Alexey on 11/19/2015.
+ */
+public class LibTest10 {
+    public static void main(String[] args){
+        Vector10 v = new Vector10();
+        List10 l = new List10();
+    }
+}
