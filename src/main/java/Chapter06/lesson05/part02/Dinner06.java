0506a5331ee0dec05a31bae4645bf8373ed3eb3e
alsamancov <alsamancov@gmail.com>
2015-11-22 21:18:45 +0200

    add classes for ch06 les5 part02

---
+++ src/main/java/Chapter06/lesson05/part02/Dinner06.java
@@ -0,0 +1,13 @@
+package Chapter06.lesson05.part02;
+
+import Chapter06.lesson05.part02.dessert.Cookie06;
+
+/**
+ * Created by Alexey on 11/22/2015.
+ */
+public class Dinner06 {
+    public static void main(String[] args) {
+        Cookie06 x = new Cookie06();
+        //x.bite();
+    }
+}
---
+++ src/main/java/Chapter06/lesson05/part02/dessert/Cookie06.java
@@ -0,0 +1,13 @@
+package Chapter06.lesson05.part02.dessert;
+
+/**
+ * Created by Alexey on 11/22/2015.
+ */
+public class Cookie06 {
+    public Cookie06(){
+        System.out.println("Cookie06 constructor");
+    }
+    void bite(){
+        System.out.println("bite");
+    }
+}
