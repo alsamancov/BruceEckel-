f46bb4fad6784db1a31e78a50fe0ad43fb25ff96
alsamancov <alsamancov@gmail.com>
2015-11-22 15:06:47 +0200

    add classes for ch06 les4 part02

---
+++ src/main/java/Chapter06/lesson04/part02/PrintTest05.java
@@ -0,0 +1,15 @@
+package Chapter06.lesson04.part02;
+
+import static Chapter06.lesson04.part02.Print05.*;
+
+/**
+ * Created by Alexey on 11/22/2015.
+ */
+public class PrintTest05 {
+    public static void main(String[] args){
+        print("Available from now on! 05");
+        print(100);
+        print(100L);
+        print(3.14159);
+    }
+}
