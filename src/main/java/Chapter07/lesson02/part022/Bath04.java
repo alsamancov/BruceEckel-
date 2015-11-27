87ab473c1a7e78a97a784df8ffa8d8c828f0a0e4
alsamancov <alsamancov@gmail.com>
2015-11-27 20:23:33 +0200

    add classes for ch07 les02 part02

---
+++ src/main/java/Chapter07/lesson02/part022/Bath04.java
@@ -0,0 +1,47 @@
+package Chapter07.lesson02.part022;
+
+/**
+ * Created by Alexey on 11/27/2015.
+ */
+class Soap04{
+    private String s;
+    Soap04(){
+        System.out.println("Soap04()");
+        s = "Constructed";
+    }
+    public String toString(){
+        return s;
+    }
+}
+public class Bath04 {
+    private String
+    s1 = "Happy",
+    s2 = "Happy",
+    s3, s4;
+    private Soap04 castille;
+    private int i;
+    private float toy;
+    public Bath04(){
+        System.out.println("Inside Bath04()");
+        s3 = "Joy";
+        toy = 3.14f;
+        castille = new Soap04();
+    }
+    { i = 47; }
+    public String toString(){
+        if(s4 == null)
+            s4 = "Joy";
+        return "s1 = " + s1 + "\n" +
+                "s2 = " + s2 + "\n" +
+                "s3 = " + s3 + "\n" +
+                "s4 = " + s4 + "\n" +
+                "i = " + i + "\n" +
+                "toy = " + toy + "\n" +
+                "castille = " + castille;
+    }
+
+    public static void main(String[] args) {
+        Bath04 b = new Bath04();
+        System.out.println(b);
+    }
+}
