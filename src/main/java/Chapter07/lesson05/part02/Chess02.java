4317413c0a0cb2eb8846e4e600c61b89080d3b62
alsamancov <alsamancov@gmail.com>
2015-11-30 21:49:38 +0200

    add classes for ch07 les05 part02

---
+++ src/main/java/Chapter07/lesson05/part02/Chess02.java
@@ -0,0 +1,25 @@
+package Chapter07.lesson05.part02;
+
+/**
+ * Created by Alexey on 11/30/2015.
+ */
+class Game02{
+    Game02(int i){
+        System.out.println("Game02 constructor");
+    }
+}
+class BoardGame02 extends Game02{
+    BoardGame02(int i){
+        super(i);
+        System.out.println("BoardGame02 constructor");
+    }
+}
+public class Chess02 extends BoardGame02{
+    Chess02(){
+        super(11);
+        System.out.println("Chess02 constructor");
+    }
+    public static void main(String[] args){
+        Chess02 x = new Chess02();
+    }
+}
