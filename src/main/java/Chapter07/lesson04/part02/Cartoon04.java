d487dec8f489832f8f67c196d1e80cf18f3809df
alsamancov <alsamancov@gmail.com>
2015-11-30 21:13:44 +0200

    add classes for ch07 les04 part02

---
+++ src/main/java/Chapter07/lesson04/part02/Cartoon04.java
@@ -0,0 +1,23 @@
+package Chapter07.lesson04.part02;
+
+/**
+ * Created by Alexey on 11/30/2015.
+ */
+class Art04{
+    Art04(){
+        System.out.println("Art04 constructor");
+    }
+}
+class Drawing04 extends Art04{
+    Drawing04(){
+        System.out.println("Drawing04 constructor");
+    }
+}
+public class Cartoon04 extends Drawing04{
+    public Cartoon04(){
+        System.out.println("Cartoon04 constructor");
+    }
+    public static void main(String[] args){
+        Cartoon04 x = new Cartoon04();
+    }
+}
