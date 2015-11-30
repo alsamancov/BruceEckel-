a8890346b5f6e249bdf383a1924c421137b82bfb
alsamancov <alsamancov@gmail.com>
2015-11-30 20:24:02 +0200

    add classes for ch07 les03 part02

---
+++ src/main/java/Chapter07/lesson03/part02/Detergent06.java
@@ -0,0 +1,47 @@
+package Chapter07.lesson03.part02;
+
+/**
+ * Created by Alexey on 11/30/2015.
+ */
+class Cleanser06{
+    private String s = "Cleanser06";
+    public void append(String a){
+        s += a;
+    }
+    public void dilute(){
+        append(" dilute()");
+    }
+    public void apply(){
+        append(" apply()");
+    }
+    public void scrub(){
+        append(" scrub()");
+    }
+    public String toString(){
+        return s;
+    }
+    public static void main(String[] args){
+        Cleanser06 x = new Cleanser06();
+        x.dilute(); x.apply(); x.scrub();
+        System.out.println(x);
+    }
+}
+public class Detergent06 extends Cleanser06{
+    public void scrub(){
+        append(" Detergent06.scrub()");
+        super.scrub();
+    }
+    public void foam(){
+        append(" foam()");
+    }
+    public static void main(String[] args){
+        Detergent06 x = new Detergent06();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        System.out.println(x);
+        System.out.println("Testing base class:");
+        Cleanser06.main(args);
+    }
+}
