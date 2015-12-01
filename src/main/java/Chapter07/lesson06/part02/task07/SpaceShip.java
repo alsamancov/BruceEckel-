6e1e0698aa875032ac7616dbda97e0aae013433c
alsamancov <alsamancov@gmail.com>
2015-12-01 14:13:50 +0200

    add Ch07 les06 part02

---
+++ src/main/java/Chapter07/lesson06/part02/task07/SpaceShip.java
@@ -0,0 +1,18 @@
+package Chapter07.lesson06.part02.task07;
+
+/**
+ * Created by Алексей on 12/1/2015.
+ */
+public class SpaceShip extends SpaceShipControls {
+    private String name;
+    public SpaceShip(String name){
+        this.name = name;
+    }
+    public String toString(){
+        return name;
+    }
+    public static void main(String[] args){
+        SpaceShip protector = new SpaceShip("NSEA Protector");
+        protector.forward(100);
+    }
+}
---
+++ src/main/java/Chapter07/lesson06/part02/task07/SpaceShipControls.java
@@ -0,0 +1,14 @@
+package Chapter07.lesson06.part02.task07;
+
+/**
+ * Created by Алексей on 12/1/2015.
+ */
+public class SpaceShipControls {
+    void up(int velocity){}
+    void down(int velocity){}
+    void left(int velocity){}
+    void right(int velocity){}
+    void forward(int velocity){}
+    void back(int velocity){}
+    void turboBoost(){}
+}
