4086837e3471f5ead33d73513c4f06a271f990fd
alsamancov <alsamancov@gmail.com>
2015-12-01 23:14:43 +0200

    add classes for ch07 les07 part02

--- src/main/java/Chapter07/lesson07/part02/PlaceSetting01.java
+++ src/main/java/Chapter07/lesson07/part02/PlaceSetting01.java
@@ -1,56 +1,56 @@
-package Chapter07.lesson07;
+package Chapter07.lesson07.part02;
 
 /**
- * Created by Alexey on 11/19/2015.
+ * Created by Alexey on 12/01/2015.
  */
 class Plate01{
     Plate01(int i){
-        System.out.println("Plate constructor");
+        System.out.println("Plate01 constructor");
     }
 }
 class DinnerPlate01 extends Plate01{
     DinnerPlate01(int i){
         super(i);
-        System.out.println("DinnerPlate constructor");
+        System.out.println("DinnerPlate01 constructor");
     }
 }
 class Utensil01{
     Utensil01(int i){
-        System.out.println("Utensil constructor");
+        System.out.println("Utensil01 constructor");
     }
 }
 class Spoon01 extends Utensil01{
     Spoon01(int i){
         super(i);
-        System.out.println("Spoon constructor");
+        System.out.println("Spoon01 constructor");
     }
 }
 class Fork01 extends Utensil01{
     Fork01(int i){
         super(i);
-        System.out.println("Fork constructor");
+        System.out.println("Fork01 constructor");
     }
 }
 class Knife01 extends Utensil01{
     Knife01(int i){
         super(i);
-        System.out.println("Knife constructor");
+        System.out.println("Knife01 constructor");
     }
 }
 class Custom01{
     Custom01(int i){
-        System.out.println("Custom constructor");
+        System.out.println("Custom01 constructor");
     }
 }
 public class PlaceSetting01 extends Custom01{
-    private Spoon01 sp;
-    private Fork01 frk;
+    private Spoon01 spoon01;
+    private Fork01 fork01;
     private Knife01 kn;
     private DinnerPlate01 pl;
     public PlaceSetting01(int i){
         super(i + 1);
-        sp = new Spoon01(i + 2);
-        frk = new Fork01(i + 3);
+        spoon01 = new Spoon01(i + 2);
+        fork01 = new Fork01(i + 3);
         kn = new Knife01(i + 4);
         pl = new DinnerPlate01(i + 5);
         System.out.println("PlaceSetting01 constructor");
