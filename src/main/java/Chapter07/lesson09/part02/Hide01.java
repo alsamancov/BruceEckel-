c58e501e2c03fab978f62d54c567442961b0a2fe
alsamancov <alsamancov@gmail.com>
2015-12-08 07:35:03 +0200

    add classes for ch07 les08 part02

---
+++ src/main/java/Chapter07/lesson09/part02/Hide01.java
@@ -0,0 +1,30 @@
+package Chapter07.lesson09.part02;
+
+/**
+ * Created by Alexey on 12/08/2015.
+ */
+class Homer01{
+    char doh(char c){
+        System.out.println("doh(char)");
+        return 'd';
+    }
+    float doh(float f){
+        System.out.println("doh(float)");
+        return 1.0f;
+    }
+}
+class Milhouse01{}
+class Bart01 extends Homer01{
+    void doh(Milhouse01 m){
+        System.out.println("doh(Milhouse01)");
+    }
+}
+public class Hide01 {
+    public static void main(String[] args){
+        Bart01 b = new Bart01();
+        b.doh(1);
+        b.doh('x');
+        b.doh(1.0f);
+        b.doh(new Milhouse01());
+    }
+}
