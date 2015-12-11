abf2a8458e5b6596ad06e94ac5459925a15df581
alsamancov <alsamancov@gmail.com>
2015-12-11 21:50:42 +0200

    add classes for ch07 les11 part02

---
+++ src/main/java/Chapter07/lesson12/part02/Wind06.java
@@ -0,0 +1,17 @@
+package Chapter07.lesson12.part02;
+
+/**
+ * Created by Alexey on 12/11/2015.
+ */
+class Instrument06{
+    public void play(){}
+    static void tune(Instrument06 i){
+        i.play();
+    }
+}
+public class Wind06 extends Instrument06{
+    public static void main(String[] args){
+        Wind06 flute = new Wind06();
+        Instrument06.tune(flute);
+    }
+}
