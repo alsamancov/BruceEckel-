5fe5def328c0a4bd3975caf3d1113e617b33d440
alsamancov <alsamancov@gmail.com>
2015-12-11 22:27:38 +0200

    add classes for ch06 les11 part03

---
+++ src/main/java/Chapter06/lesson01/SingleImport21.java
@@ -0,0 +1,12 @@
+package Chapter06.lesson01;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alexey on 12/11/2015.
+ */
+public class SingleImport21 {
+    public static void main(String[] args){
+        ArrayList list = new java.util.ArrayList();
+    }
+}
