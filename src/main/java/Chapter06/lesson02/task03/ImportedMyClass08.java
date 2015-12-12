c16d62e4100b08e9176f36c3723897863ad20b28
alsamancov <alsamancov@gmail.com>
2015-12-12 18:18:48 +0200

    add classes for ch06 les11 part03

---
+++ src/main/java/Chapter06/lesson02/task03/ImportedMyClass08.java
@@ -0,0 +1,13 @@
+package Chapter06.lesson02.task03;
+
+import Chapter06.lesson02.task03.mypackage.MyClass08;
+
+/**
+ * Created by Alexey on 12/12/2015.
+ */
+public class ImportedMyClass08 {
+    public static void main(String[] args){
+        MyClass08 m = new MyClass08();
+        System.out.println(m);
+    }
+}
