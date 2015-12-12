60120a5b75514932d2b30f61013bf135f84dbfd6
alsamancov <alsamancov@gmail.com>
2015-12-12 19:18:44 +0200

    add classes for ch06 les03 part03

---
+++ src/main/java/Chapter06/lesson03/net/mindview/simple03/LibTest06.java
@@ -0,0 +1,11 @@
+package Chapter06.lesson03.net.mindview.simple03;
+
+/**
+ * Created by Alexey on 12/12/2015.
+ */
+public class LibTest06 {
+    public static void main(String[] args){
+        Vector06 vector06 = new Vector06();
+        List06 list06 = new List06();
+    }
+}
---
+++ src/main/java/Chapter06/lesson03/net/mindview/simple03/Vector06.java
@@ -0,0 +1,10 @@
+package Chapter06.lesson03.net.mindview.simple03;
+
+/**
+ * Created by Alexey on 12/12/2015.
+ */
+public class Vector06 {
+    public Vector06(){
+        System.out.println("Chapter06.lesson03.net.mindview.simple03.Vector06");
+    }
+}
