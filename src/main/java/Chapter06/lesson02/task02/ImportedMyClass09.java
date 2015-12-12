67541197580b3ab103aff6c60a04a52b9e4c3097
alsamancov <alsamancov@gmail.com>
2015-12-12 07:46:43 +0200

    add classes for ch06 les11 part03

---
+++ src/main/java/Chapter06/lesson02/task02/ImportedMyClass09.java
@@ -0,0 +1,12 @@
+package Chapter06.lesson02.task02;
+
+import Chapter06.lesson02.task02.mypackage.MyClass09;
+
+/**
+ * Created by Alexey on 12/12/2015.
+ */
+public class ImportedMyClass09 {
+    public static void main(String[] args){
+        MyClass09 m = new MyClass09();
+    }
+}
---
+++ src/main/java/Chapter06/lesson02/task02/QualifiedMyClass09.java
@@ -0,0 +1,11 @@
+package Chapter06.lesson02.task02;
+
+/**
+ * Created by Alexey on 12/12/2015.
+ */
+public class QualifiedMyClass09 {
+    public static void main(String[] args){
+        Chapter06.lesson02.task02.mypackage.MyClass09 m =
+                new Chapter06.lesson02.task02.mypackage.MyClass09();
+    }
+}
