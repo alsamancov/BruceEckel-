34a18599c78dc8c6447254d54e1effe3875c8048
alsamancov <alsamancov@gmail.com>
2015-12-14 22:37:44 +0200

    add classes for ch06 les05 part03

---
+++ src/main/java/Chapter06/lesson05/part03/dessert/Cookie02.java
@@ -0,0 +1,13 @@
+package Chapter06.lesson05.part03.dessert;
+
+/**
+ * Created by Alexey on 12/14/2015.
+ */
+public class Cookie02 {
+    public Cookie02(){
+        System.out.println("Cookie02 constructor");
+    }
+    void bite(){
+        System.out.println("bite");
+    }
+}
