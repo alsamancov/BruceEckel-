16e34cf45414fd74c5d532e9907aa70102371450
alsamancov <alsamancov@gmail.com>
2015-12-15 22:35:20 +0200

    add classes for ch06 les06 part03

---
+++ src/main/java/Chapter06/lesson06/part03/Cake06.java
@@ -0,0 +1,11 @@
+package Chapter06.lesson06.part03;
+
+/**
+ * Created by Alexey on 12/15/2015.
+ */
+public class Cake06 {
+    public static void main(String[] args){
+        Pie06 x = new Pie06();
+        x.f();
+    }
+}
