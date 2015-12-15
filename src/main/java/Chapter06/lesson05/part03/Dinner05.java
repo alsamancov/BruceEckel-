b385982b427986efa0301e008ab17d47a339e40c
alsamancov <alsamancov@gmail.com>
2015-12-15 21:10:49 +0200

    add classes for ch06 les05 part03

---
+++ src/main/java/Chapter06/lesson05/part03/Dinner05.java
@@ -0,0 +1,13 @@
+package Chapter06.lesson05.part03;
+
+import Chapter06.lesson05.part03.dessert.Cookie05;
+
+/**
+ * Created by Alexey on 12/15/2015.
+ */
+public class Dinner05 {
+    public static void main(String[] args){
+        Cookie05 x = new Cookie05();
+        //x.bite();
+    }
+}
