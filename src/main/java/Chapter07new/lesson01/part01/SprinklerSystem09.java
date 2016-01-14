b757c921261fb3c48597299a69dce25cd7783832
alsamancov <alsamancov@gmail.com>
2016-01-14 21:05:07 +0200

    add classes for ch07new les01 part01

---
+++ src/main/java/Chapter07new/lesson01/part01/SprinklerSystem09.java
@@ -0,0 +1,33 @@
+package Chapter07new.lesson01.part01;
+
+/**
+ * Created by Alexey on 01/14/2016.
+ */
+class WaterSource09{
+    private String s;
+    WaterSource09(){
+        System.out.println("WaterSource09()");
+        s = "Constructed09";
+    }
+    public String toString(){
+        return s;
+    }
+}
+public class SprinklerSystem09 {
+    private String valve1, valve2, valve3, valve4;
+    private WaterSource09 source09 = new WaterSource09();
+    private int i;
+    private float f;
+    public String toString(){
+        return "valve1 = " + valve1 + " " +
+                "valve2 = " + valve2 + " " +
+                "valve3 = " + valve3 + " " +
+                "valve4 = " + valve4 + "\n" +
+                "i = " + i + " " + "f = " + f + " " +
+                "source = " + source09;
+    }
+    public static void main(String[] args){
+        SprinklerSystem09 sprinklerSystem09 = new SprinklerSystem09();
+        System.out.println(sprinklerSystem09);
+    }
+}
